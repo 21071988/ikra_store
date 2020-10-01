a49b2fe6d94712771d916dfc149aa8401e6a800c
21071988 <shaparev1988@mail.ru>
2020-10-01 18:02:25 +1000

    catalog which doesn't work

---
+++ catalog/index.php
@@ -0,0 +1,42 @@
+<?php require_once('./header.php');?>
+<div class="catalog">
+  <div class="menu">
+    <div class="menu__categories">
+      <a href="">Платья</a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+    </div>
+    <div class="menu__brands">
+    
+    </div>
+  </div>
+  <div class="catalog__part">
+    <div class="top__menu">
+    
+    </div>
+    <div class="catalog__itself">
+    
+    </div>
+    <div class="catalog__pages">
+    
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<?php require_once('./footer.php');?>
+
+
