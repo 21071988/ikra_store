28e391bc8e9d7ad70ad665d2dfb5799b24ec4f6a
21071988 <shaparev1988@mail.ru>
2020-10-01 18:08:56 +1000

    Update index.php

--- catalog/index.php
+++ catalog/index.php
@@ -1,22 +1,42 @@
-<?php require_once('./header.php');?>
+
 <div class="catalog">
   <div class="menu">
     <div class="menu__categories">
       <a href="">Платья</a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
+      <a href="">топ</a>
+      <a href="">юбки</a>
+      <a href="">джинсы</a>
+      <a href="">брюки</a>
+      <a href="">аксессуары</a>
+      <a href="">обувь</a>
+      <a href="">пальто</a>
+      <a href="">верхняя одежда</a>
+      <a href="">аксессуары</a>
+      <a href="">юбки</a>
+      <a href="">джинсы</a>
+      <a href="">брюки</a>
+      <a href="">аксессуары</a>
+      <a href="">обувь</a>
+      <a href="">пальто</a>
     </div>
     <div class="menu__brands">
-    
+    <b>
+    <a href="">Платья</a>
+      <a href="">топ</a>
+      <a href="">юбки</a>
+      <a href="">джинсы</a>
+      <a href="">брюки</a>
+      <a href="">аксессуары</a>
+      <a href="">обувь</a>
+      <a href="">пальто</a>
+      <a href="">верхняя одежда</a>
+      <a href="">аксессуары</a>
+      <a href="">юбки</a>
+      <a href="">джинсы</a>
+      <a href="">брюки</a>
+      <a href="">аксессуары</a>
+      <a href="">обувь</a>
+      <a href="">пальто</a>
     </div>
   </div>
   <div class="catalog__part">
